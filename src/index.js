977b1c63bc9596d5ec4ef931fa6c54e43f412403
Andrey <andriiano@yandex.ru>
2023-01-25 14:51:50 +0300

    feat: add projec

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let number;
+    if(n < 0) {
+       number = -n
+    } else {
+      number = n
+    }
+    let arr = String(number).split("").reverse().join('')
+    return Number(arr)
 }
